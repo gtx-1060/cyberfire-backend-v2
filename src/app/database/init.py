b2d12fb4f9f4347e34cb287a69574f134498b9dd
Vlad Nenov <xrapovva@mail.ru>
2021-07-29 16:42:57 +0300

    drop tables alias

--- src/app/database/init.py
+++ src/app/database/init.py
@@ -9,9 +9,16 @@ from src.app.models.stage import Stage
 from src.app.models.tournament import Tournament
 from src.app.models.stats import MatchStats, TournamentStats, GlobalStats
 
+my_tables = [User.__table__, Squad.__table__, News.__table__, Stage.__table__, Tournament.__table__,
+             MatchStats.__table__, TournamentStats.__table__, GlobalStats.__table__]
+
 
 def create_tables():
     engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
-    my_tables = [User.__table__, Squad.__table__, News.__table__, Stage.__table__, Tournament.__table__,
-                 MatchStats.__table__, TournamentStats.__table__, GlobalStats.__table__]
     Base.metadata.create_all(engine, tables=my_tables)
+
+
+def run_drop_tables():
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
+    Base.metadata.drop_all(engine, tables=my_tables)
+
